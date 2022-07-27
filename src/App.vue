51a87bbb76840f825e2eea30f454f120e6cdf6e2
gtipan <gtipan@mit.edu>
2022-07-26 22:58:49 -0700

    first commit add

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <template>
+  <div id="nav">
+  <nav class="navbar navbar-expand-lg">
+    <a class="navbar-brand" href="#">Logo Here</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="navbar-nav mr-auto">
+            <router-link to="/" class="nav-item nav-link">Home</router-link>
+            <router-link to="/About" class="nav-item nav-link">About</router-link>
+            <router-link to="/Careers" class="nav-link">Careers</router-link>
+            <router-link to="/Contact" class="nav-link">Contact</router-link>
+        </div>
+      </div>
+  </nav>
+  </div>
+    <router-view/>
+  </template>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -23,6 +31,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
